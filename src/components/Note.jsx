9659434395872a1313380c7c3e6ebc530ae2b653
unknown <it21039386@my.sliit.lk>
2023-06-29 13:22:21 +0530

    initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Note() {
+  return (
+    <div>
+    <div className="note">
+      <h1>Title Of The Note</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse.</p>
+    </div>
+    <div className="note">
+    <h1>Title Of The Note</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse.</p>
+  </div>
+  </div>
+  );
+}
